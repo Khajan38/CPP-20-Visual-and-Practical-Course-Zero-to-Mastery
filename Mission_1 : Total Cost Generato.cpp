cc716c47a1ba40339dfddfddd1760d0eee5db95f
khajan_bhatt <140534285+Khajan38@users.noreply.github.com>
2024-07-29 20:28:42 +0530

    Create Mission_1 : Total Cost Generato.cpp

---
+++ Mission_1 : Total Cost Generato.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+//Make a Total_Cost_Generator
+int main(){
+     float SALES_TAX_RATE {0.08}, Price;
+     int Quantity;
+     std::cout<<"\nEnter the quantity : "; std::cin>>Quantity;
+     cout<<"Enter the price : "; cin>>Price;
+     auto Subtotal = Quantity * Price;
+     float SalesTax = Subtotal * SALES_TAX_RATE;
+     float TotalCost = Subtotal + SalesTax;
+     cout<<"\nSubtotal : "<<Subtotal
+         <<", Sales Tax : "<<SalesTax
+         <<", Total Cost : "<<TotalCost<<endl;
+     return 0;
+}
